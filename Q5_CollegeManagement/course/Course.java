6ba02f8a7d63344ea8b586b8cd5c1907916a7ee3
sajankrishna31-eng <sajankrishna31@gmail.com>
2026-09-17 13:34:12 +0530

    Add Tutorial 6 Question 5 Course package

---
+++ Q5_CollegeManagement/course/Course.java
@@ -0,0 +1,16 @@
+package course;
+
+public class Course {
+    private String courseCode;
+    private String courseName;
+
+    public Course(String courseCode, String courseName) {
+        this.courseCode = courseCode;
+        this.courseName = courseName;
+    }
+
+    public void displayDetails() {
+        System.out.println("Course Code: " + courseCode);
+        System.out.println("Course Name: " + courseName);
+    }
+}
